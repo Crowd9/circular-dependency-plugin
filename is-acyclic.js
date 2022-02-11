17cffad5cc7ceae436a2f7a8f0feb96b83d15ce1
Nikita Skazki <nskazki@gmail.com>
2022-02-11 20:51:33 +0300

    Make the fast cycle checker abide the exclude option

--- is-acyclic.js
+++ is-acyclic.js
@@ -19,10 +19,11 @@
  *
  * See https://stackoverflow.com/questions/261573/best-algorithm-for-detecting-cycles-in-a-directed-graph
  */
-function isAcyclic(graph) {
+function isAcyclic(graph, options) {
   let isAcyclic = true;
   depthFirstIterator(
     graph,
+    options,
     () => {},
     (backEdge) => isAcyclic = false);
   return isAcyclic;
@@ -35,27 +36,35 @@ function isAcyclic(graph) {
  * The visitor function is called with vertex, adjacent vertices
  * The backEdge function is called with head, tail of a back edge
  */
-function depthFirstIterator(graph, visitorFn, backEdgeFn) {
+function depthFirstIterator(graph, options, visitorFn, backEdgeFn) {
   const discovered = new Set();
   const finished = new Set();
   for (const vertex of graph.vertices) {
     if (!(discovered.has(vertex) || finished.has(vertex)))
-      depthFirstVisitor(vertex, discovered, finished, graph, visitorFn, backEdgeFn)
+      depthFirstVisitor(vertex, discovered, finished, graph, options, visitorFn, backEdgeFn)
   }
 }
 
 
-function depthFirstVisitor(vertex, discovered, finished, graph, visitorFn, backEdgeFn) {
+function depthFirstVisitor(vertex, discovered, finished, graph, options, visitorFn, backEdgeFn) {
   discovered.add(vertex)
   const adjacent = graph.arrow(vertex);  // the adjacent vertices in the direction of the edges
   visitorFn(vertex, adjacent);
 
   for (const v of adjacent) {
+    const shouldCheck = v.resource != null
+      && options.include.test(v.resource)
+      && !options.exclude.test(v.resource)
+
+    if (!shouldCheck) {
+      continue
+    }
+
     if (discovered.has(v)) {
       backEdgeFn(vertex, v);
     } else {
       if (!finished.has(v))
-        depthFirstVisitor(v, discovered, finished, graph, visitorFn, backEdgeFn)
+        depthFirstVisitor(v, discovered, finished, graph, options, visitorFn, backEdgeFn)
     }
   }
   discovered.delete(vertex)
